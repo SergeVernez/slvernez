964c3c59acf0704b93732a37bff02cdd0b924b2c
SergeVernez <sergevernez.pro@gmail.com>
2025-01-30 16:40:58 +0100

    Ajout animation js nav

--- header.js
+++ header.js
@@ -1,8 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
+	// Charger le header
 	fetch("header.html")
 		.then((response) => response.text())
 		.then((data) => {
 			document.getElementById("header-container").innerHTML = data;
+
+			// Ajouter la classe active au lien de la page
+			const currentPath = window.location.pathname.split("/").pop();
+			const navLinks = document.querySelectorAll("nav ul li a");
+
+			navLinks.forEach((link) => {
+				if (link.getAttribute("href") === currentPath) {
+					link.classList.add("active"); // Ajouter la classe "active" au lien correspondant
+				}
+			});
 		})
 		.catch((error) => {
 			console.error("Erreur du chargement header: ", error);
