7d2de1d5efdb98895afd109a4c76d888a6823faa
SergeVernez <sergevernez.pro@gmail.com>
2025-01-30 17:18:05 +0100

    fix modif

--- header.js
+++ header.js
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
-	// Charger le header
-	fetch("header.html")
-		.then((response) => response.text())
-		.then((data) => {
-			document.getElementById("header-container").innerHTML = data;
+    // Charger le header
+    fetch("header.html")
+        .then((response) => response.text())
+        .then((data) => {
+            document.getElementById("header-container").innerHTML = data;
 
-			// Ajouter la classe active au lien de la page
-			const currentPath = window.location.pathname.split("/").pop();
-			const navLinks = document.querySelectorAll("nav ul li a");
+            // Ajouter la classe active au lien correspondant
+            const currentPath = window.location.pathname.split('/').pop();
+            const navLinks = document.querySelectorAll('nav ul li a');
 
-			navLinks.forEach((link) => {
-				if (link.getAttribute("href") === currentPath) {
-					link.classList.add("active"); // Ajouter de la classe css "active" au lien correspondant
-				}
-			});
-		})
-		.catch((error) => {
-			console.error("Erreur du chargement header: ", error);
-		});
+            navLinks.forEach(link => {
+                if (link.getAttribute('href') === currentPath) {
+                    link.classList.add('active'); // Ajouter la classe "active" au lien correspondant
+                }
+            });
+        })
+        .catch((error) => {
+            console.error("Erreur du chargement header: ", error);
+        });
 });
