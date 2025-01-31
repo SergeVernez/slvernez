4263c9a5acf1fc037989a8759a8731ebc3f6e154
SergeVernez <sergevernez.pro@gmail.com>
2025-01-31 16:18:34 +0100

    integration des fonts via js

---
+++ loadFonts.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const link = document.createElement("link");
+	link.rel = "stylesheet";
+	link.href = "https://fonts.googleapis.com/css2?family=Economica:ital,wght@0,400;0,700;1,400;1,700&family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&family=Roboto+Flex:opsz,wght@8..144,100..1000&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900&display=swap";
+
+	document.head.appendChild(link);
+});
