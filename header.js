9d7e37a328dfd0ee6aba6110cc67d66893cad7a0
SergeVernez <sergevernez.pro@gmail.com>
2025-01-31 10:11:00 +0100

    ajout scroll top

--- header.js
+++ header.js
@@ -1,21 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Charger le header
-    fetch("header.html")
-        .then((response) => response.text())
-        .then((data) => {
-            document.getElementById("header-container").innerHTML = data;
+	// Charger le header
+	fetch("header.html")
+		.then((response) => response.text())
+		.then((data) => {
+			document.getElementById("header-container").innerHTML = data;
 
-            // Ajouter la classe active au lien correspondant
-            const currentPath = window.location.pathname.split('/').pop();
-            const navLinks = document.querySelectorAll('nav ul li a');
+			// Ajouter la classe active au lien correspondant
+			const currentPath = window.location.pathname.split("/").pop();
+			const navLinks = document.querySelectorAll("nav ul li a");
 
-            navLinks.forEach(link => {
-                if (link.getAttribute('href') === currentPath) {
-                    link.classList.add('active'); // Ajouter la classe "active" au lien correspondant
-                }
-            });
-        })
-        .catch((error) => {
-            console.error("Erreur du chargement header: ", error);
-        });
+			navLinks.forEach((link) => {
+				if (link.getAttribute("href") === currentPath) {
+					link.classList.add("active"); // Ajouter la classe "active" au lien correspondant
+				}
+			});
+			// ajout scroll top
+			window.onscroll = function () {
+				scrollFunction();
+			};
+
+			function scrollFunction() {
+				const scrollTopBtn = document.getElementById("scrollTopBtn");
+				if (document.body.scrollTop > 200 || document.getElement.scrollFunction > 200) {
+					scrollTopBtn.style.display = "block";
+				} else {
+					scrollTopBtn.style.display = "none";
+				}
+			}
+
+			// gestion pour le click
+			document.getElementById("scrollTopBtn").addEventListener("click", function () {
+				document.body.scrollTop = 0; //pour Apple
+				document.documentElement.scrollTop = 0;
+			});
+		})
+		.catch((error) => {
+			console.error("Erreur du chargement header: ", error);
+		});
 });
