3658ff631dd228f4aaf4ddff24a249673cf36e8e
SergeVernez <sergevernez.pro@gmail.com>
2025-03-19 09:22:36 +0100

    modif server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 const nodmailer = require('nodemailer');
+
+//configuration du transporteur
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'slvfrontdesign@gmail.com',
+        pass: ''
+    }
+});
+
+//fonction pour envoyer una mail
