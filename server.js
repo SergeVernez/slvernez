52c113702c56d3d6936577bfcc8bcfe695c4e236
SergeVernez <sergevernez.pro@gmail.com>
2025-03-31 11:44:46 +0200

    nouveau commit nouvelle machine git clone

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
-const nodmailer = require('nodemailer');
+const nodemailer = require('nodemailer');
 
-//configuration du transporteur
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'slvfrontdesign@gmail.com',
-        pass: ''
-    }
+  service: 'gmail', // ou un autre service (ex : outlook, yahoo)
+  auth: {
+    user: 'votre_email@gmail.com',
+    pass: 'votre_mot_de_passe', // Attention : utilisez des variables d'environnement pour des raisons de sécurité !
+  },
 });
 
-//fonction pour envoyer una mail
+const mailOptions = {
+  from: 'votre_email@gmail.com',
+  to: 'destinataire@gmail.com',
+  subject: 'Sujet de l’email',
+  text: 'Bonjour, ceci est un test envoyé avec Nodemailer !',
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.log('Erreur :', error);
+  } else {
+    console.log('Email envoyé : ' + info.response);
+  }
+});
