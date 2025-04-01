80956525c34eb8210a9a1694ce4a9af8437e6a2f
SergeVernez <sergevernez.pro@gmail.com>
2025-04-01 09:40:43 +0200

    modif responsive footer

--- loadFonts.js
+++ loadFonts.js
@@ -4,27 +4,9 @@ document.addEventListener("DOMContentLoaded", function () {
 	link.href = "https://fonts.googleapis.com/css2?family=Economica:ital,wght@0,400;0,700;1,400;1,700&family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&family=Roboto+Flex:opsz,wght@8..144,100..1000&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900&display=swap";
 
 	document.head.appendChild(link);
+
 });
-/* .economica-regular {
-  font-family: "Economica", serif;
-  font-weight: 400;
-  font-style: normal;
-}
 
-.economica-bold {
-  font-family: "Economica", serif;
-  font-weight: 700;
-  font-style: normal;
-}
-
-.economica-regular-italic {
-  font-family: "Economica", serif;
-  font-weight: 400;
-  font-style: italic;
-}
-
-.economica-bold-italic {
-  font-family: "Economica", serif;
-  font-weight: 700;
-  font-style: italic;
-} */
+document.fonts.ready.then(() => {
+  document.body.style.fontFamily = 'Roboto, sans-serif';
+});
